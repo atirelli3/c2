f187b038e7b23461b46b8894b88d4dd2a42bd9a1
Andrea Tirelli <ta.tirelliandrea@gmail.com>
2024-10-13 00:39:42 +0200

    code clean up

--- .modules/new/0-preparation.sh
+++ .modules/new/0-preparation.sh
@@ -26,11 +26,9 @@ eval "timedatectl set-ntp true $redir_output"  # Enable NTP for time synchroniza
 
 # Mirrorlist
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup  # Backup current mirrorlist
-
 # Check if 'mirrorcountries' is declared and sanitize into a string for 'reflector'
 # Otherwise empty string (no countries provided)
 reflector_countries=$(declare -p mirrorcountries &>/dev/null && IFS=, echo "${mirrorcountries[*]}" || echo "")
-
 # Execute reflector to generate a new mirrorlist
 # (if countries are provided use them w/ '--country', otherwise use default)
 eval "reflector ${reflector_countries:+--country "$reflector_countries"} \
@@ -38,7 +36,6 @@ eval "reflector ${reflector_countries:+--country "$reflector_countries"} \
           --age 6 \
           --sort rate \
           --save /etc/pacman.d/mirrorlist $redir_output"
-
 eval "pacman -Syy $redir_output"  # Refresh package manager database(s)
 
 # Configure pacman:
@@ -51,7 +48,6 @@ sed -i "/etc/pacman.conf" \
     -e "s|^#Color|&\nColor\nILoveCandy|" \
     -e "s|^#VerbosePkgLists|&\nVerbosePkgLists|" \
     -e "s|^#ParallelDownloads.*|&\nParallelDownloads = 20|"
-
 eval "pacman -Syy $redir_output"  # Refresh package manager database(s)
 
 # Keyring(s)
